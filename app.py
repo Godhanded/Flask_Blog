b72855387bfa070a468e66dd5853885f79cdaed3
Godand <aezeigwe2003@gmail.com>
2023-03-06 08:04:48 -0500

    posts templates

--- app.py
+++ app.py
@@ -3,34 +3,32 @@ import json
 
 app=Flask(__name__)
 
-data=[
-    {
-        "user":"test user",
-        "body":"bshdkcohjbdjshybgjshcjbdhsjchuychdhbgfjhshiybcdhsjhsjuhdjssdgtchd\
-        sdhdsjdkjlujgsdjhsjuhygjdshjhbydhsjhdybdsjibhdsjjihdbhsdsbhdn",
-        "author":"Adams",
-        "date":"12/07/2023",
-    },
 
+posts = [
     {
-        "user":"test user",
-        "body":"bshdkcohjbdjshybgjshcjbdhsjchuychdhbgfjhshiybcdhsjhsjuhdjssdgtchd\
-        sdhdsjdkjlujgsdjhsjuhygjdshjhbydhsjhdybdsjibhdsjjihdbhsdsbhdn",
-        "author":"Kate",
-        "date":"13/07/2023",
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html",datas=data)
+    return render_template("home.html",posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 if (__name__== __name__):
     app.debug=True
-    app.run()
+    app.run(debug=True)

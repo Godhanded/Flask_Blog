8ae82d31552e20778ecee7db58221b9574957b9d
Godand <aezeigwe2003@gmail.com>
2023-03-09 23:34:47 -0500

    downgrade itsdangerous|JWS function to user model

--- flask_blog/models.py
+++ flask_blog/models.py
@@ -1,4 +1,5 @@
-from flask_blog import db, login_manager, query_all_filtered, query_one_filtered
+from flask_blog import db, login_manager, query_all_filtered, query_one_filtered,app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
 from flask_login import UserMixin
 
@@ -24,6 +25,19 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
+    
+    def get_reset_token(self,expires_sec=1800):
+        s=Serializer(app.config['SECRET_KEY'],expires_sec)
+        return s.dumps({'user_id':self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s=Serializer(app.config['SECRET_KEY'],expires_sec)
+        try:
+            user_id=s.loads(token)['user_id']
+        except:
+            return None
+        return query_one_filtered(User, id=user_id)
 
     def insert(self):
         db.session.add(self)

f2cfde6b36393348b607d4ef78cc4bd36fd6b785
Godand <aezeigwe2003@gmail.com>
2023-03-10 17:44:35 -0500

    set up dotenv

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "54321"
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
@@ -22,8 +26,8 @@ mail_settings = {
     "MAIL_PORT": 587,
     "MAIL_USE_TLS": True,
     "MAIL_USE_SSL": False,
-    "MAIL_USERNAME": "edwardedd2003@gmail.com",
-    "MAIL_PASSWORD": "xbiowhtagfhvymox",
+    "MAIL_USERNAME": os.environ.get('USER_NAME'),
+    "MAIL_PASSWORD": os.environ.get('PASS'),
 }
 app.config.update(mail_settings)
 mail = Mail(app)
